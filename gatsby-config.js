7ad42695ee99db0a67952b6c35435a936ae7566b
NRD89 <ndaltondev@gmail.com>
2019-10-14 15:39:54 -0700

    updated display in gatsby-plugin-manifest to standalone

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,12 +27,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `THC-Kiosk-App`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
